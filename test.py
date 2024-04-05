f740530c657fdf87424484885e919571b197d16a
loptr-yoo <92285086+loptr-yoo@users.noreply.github.com>
2024-04-05 20:20:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import ply.lex as lex
-#预处理
-
+import sys
 #lex
 class Lexer:
     error=[]
-    ans = list()
-    def _init_(self):
-        reserved = {
+    reserved = {
             'program': 'PROGRAM',
             'const': 'CONST',
             'type': 'TYPE',
@@ -41,33 +38,33 @@ class Lexer:
             'true': 'TRUE',
             'false': 'FALSE',
         }
-        reserved_2={
+    reserved_2={
             'div': 'MULOP',
             'mod': 'MULOP',
             'and': 'MULOP',
             'or': 'ADDOP',
         }
 
-        tokens = ['DIGITS','NUM','LETTERS','RELOP','ADDOP','MULOP','ID','ASSIGNOP','COMMENT','DOT','COLON','LBRACKET','RBRACKET','LPAREN','RPAREN',
+    tokens = ['DIGITS','NUM','LETTERS','RELOP','ADDOP','MULOP','ID','ASSIGNOP','COMMENT','DOT','COLON','LBRACKET','RBRACKET','LPAREN','RPAREN',
                 'COM','POINT','SEMICOLON','EQUAL'
                 ] + list(reserved.values())
 
-        t_EQUAL=r'='
-        t_COLON = r':'
-        t_LBRACKET = r'\['
-        t_RBRACKET = r'\]'
-        t_LPAREN = r'\('
-        t_RPAREN = r'\)'
-        t_COM = r','
-        t_POINT = r'\.'
-        t_DOT=r'\.\.'
-        t_SEMICOLON = r';'
-        t_ASSIGNOP=r':='
-        t_RELOP = r'<=|>=|<>|<|>'
-        t_ADDOP = r'(?i)\+|-'
-        t_MULOP = r'(?i)\*|\/'
-        t_DIGITS=r'\d+'
-        def t_LETTERS(t):
+    t_EQUAL=r'='
+    t_COLON = r':'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_COM = r','
+    t_POINT = r'\.'
+    t_DOT=r'\.\.'
+    t_SEMICOLON = r';'
+    t_ASSIGNOP=r':='
+    t_RELOP = r'<=|>=|<>|<|>'
+    t_ADDOP = r'(?i)\+|-'
+    t_MULOP = r'(?i)\*|\/'
+    t_DIGITS=r'\d+'
+    def t_LETTERS(self,t):
                 r'\'[^\']*\'*'
                 t.value=t.value[1:] if len(t.value)!=1 else 'eof' #获取引号内内容
                 if t.value[-1]!='\'' or t.value=='eof': #引号未关闭
@@ -103,9 +100,9 @@ class Lexer:
                     t.value=t.value[:-1]
                 return t
         
-        def t_ID(t):
+    def t_ID(self,t):
             r'[0-9a-zA-Z_][a-zA-Z_0-9]*'
-            t.type = dict(reserved, **reserved_2).get(t.value,'ID')    # Check for reserved words
+            t.type = dict(self.reserved, **self.reserved_2).get(t.value,'ID')    # Check for reserved words
 
             if t.value.isdigit():
                     t.type='DIGITS'
@@ -133,7 +130,7 @@ class Lexer:
                                 t.value = t.value[1:]  # 错误恢复：如果ID首元素是数字，则去掉该数字
             return t
 
-        def t_COMMENT(t):
+    def t_COMMENT(self,t):
             r'\{[^{}]*\}*'
             t.lexer.lineno += t.value.count('\n')
             if t.value[-1]!='}':
@@ -149,16 +146,12 @@ class Lexer:
             # No return value. Token discarded
 
         # Define a rule so we can track line numbers
-        def t_newline(t):
+    def t_newline(self,t):
             r'\n+'
             t.lexer.lineno += t.value.count('\n')
 
-        # Compute column.
-        # input is the input text string
-        # token is a token instance
-
         # Error handling rule
-        def t_error(t):
+    def t_error(self,t):
                     self.error.append({  # 不在已有错误中，则为词法分析中的非法字符错误
                         "code": "A-02",
                         "info": {
@@ -169,40 +162,39 @@ class Lexer:
                     })
                     t.lexer.skip(1)  # 错误处理：跳过该错误
 
-        t_ignore  = ' \t'
-        # Build the lexer
-        lexer = lex.lex()
-    def find_column(data,token):
+    t_ignore  = ' \t'
+    def build(self,**kwargs):
+            self.lexer = lex.lex(module=self, **kwargs)
+    def find_column(self,data,token,**kwargs):
             last_cr = data.rfind('\n',0,token.lexpos)
             if last_cr < 0:
                 last_cr = 0
             column = token.lexpos - last_cr
             return column
-    def Lexical(self,filename):
+    
+    def debug(self,filename,debug):
         # Build the lexer
-        lexer = lex.lex()
-        filename = filename + ".pas"
-        data = '''
-        const {vfsab
-            b='
-            c='a
-    '''
-        # Give the lexer some input
-        lexer.input(data)
+        with open(filename,'r',encoding='utf-8') as file: # input
+            data=file.read().replace('\xa0',' ')#转空格
+            self.lexer.input(data.lower()) #转小写
         # Tokenize
-        while True:
-            tok = lexer.token()
-            if not tok:
-                    break      # No more input
+        print(f"{'TOKEN':^20}\n{'type':<10} {'value':<8} {'lineno':<3}")
+        for tok in self.lexer:
+            if self.find_column(data, tok)  < 1000:
+                print(f'{tok.type:<10} {tok.value:<8} {tok.lineno:<3}')
             else:
-                if self.find_column(data, tok)  < 1000:
-                    self.ans.append(tok)
-                else:
-                    self.error.append({
-                        "code": "A-02",
-                        "info": {
-                            "line": 0,
-                            "value": ["代码行长度过长"],
-                            "lexpos": 0}
-                            })
-                    exit()
+                self.error.append({
+                    "code": "A-02",
+                    "info": {
+                        "line": 0,
+                        "value": ["代码行长度过长"],
+                        "lexpos": 0}
+                        })
+        print(f"{'ERROR':^20}")
+        for item in self.error:
+             print(f"{item['code']!s:<10}  {item['info']['line']!s:<3} {item['info']['value']!s:<10}")
+if __name__ == '__main__':
+     lexi=Lexer()
+     lexi.build()
+     lexi.debug('lex_test\comment.pas',1)
+     #lexi.debug(sys.argv[1],1)
