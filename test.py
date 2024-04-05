02ec4edb3615d5963caa3ea909aa09d09f17252c
loptr-yoo <92285086+loptr-yoo@users.noreply.github.com>
2024-04-05 18:10:35 +0800

    Update test.py
    司汤达

--- test.py
+++ test.py
@@ -1,123 +1,208 @@
 import ply.lex as lex
+#预处理
 
-reserved = {
-    'program': 'PROGRAM',
-    'const': 'CONST',
-    'type': 'TYPE',
-    'record': 'RECORD',
-    'array': 'ARRAY',
-    'of': 'OF',
-    'var': 'VAR',
-    'function': 'FUNCTION',
-    'procedure': 'PROCEDURE',
-    'begin': 'BEGIN',
-    'end': 'END',
-    'if': 'IF',
-    'then': 'THEN',
-    'else': 'ELSE',
-    'case': 'CASE',
-    'while': 'WHILE',
-    'repeat': 'REPEAT',
-    'until': 'UNTIL',
-    'for': 'FOR',
-    'to': 'TO',
-    'downto': 'DOWNTO',
-    'do': 'DO',
-    'read': 'READ',
-    'write': 'WRITE',
-    'readIn': 'READIN',
-    'writeIn': 'WRITEIN',
-    'not': 'NOT',
-    'integer': 'INTERGER',
-    'real': 'REAL',
-    'boolean': 'BOOLEAN',
-    'char': 'CHAR',
-    'true': 'TRUE',
-    'false': 'FALSE',
-}
+#lex
+class Lexer:
+    error=[]
+    ans = list()
+    def _init_(self):
+        reserved = {
+            'program': 'PROGRAM',
+            'const': 'CONST',
+            'type': 'TYPE',
+            'record': 'RECORD',
+            'array': 'ARRAY',
+            'of': 'OF',
+            'var': 'VAR',
+            'function': 'FUNCTION',
+            'procedure': 'PROCEDURE',
+            'begin': 'BEGIN',
+            'end': 'END',
+            'if': 'IF',
+            'then': 'THEN',
+            'else': 'ELSE',
+            'case': 'CASE',
+            'while': 'WHILE',
+            'repeat': 'REPEAT',
+            'until': 'UNTIL',
+            'for': 'FOR',
+            'to': 'TO',
+            'downto': 'DOWNTO',
+            'do': 'DO',
+            'read': 'READ',
+            'write': 'WRITE',
+            'readIn': 'READIN',
+            'writeIn': 'WRITEIN',
+            'not': 'NOT',
+            'integer': 'INTEGER',
+            'real': 'REAL',
+            'boolean': 'BOOLEAN',
+            'char': 'CHAR',
+            'true': 'TRUE',
+            'false': 'FALSE',
+        }
+        reserved_2={
+            'div': 'MULOP',
+            'mod': 'MULOP',
+            'and': 'MULOP',
+            'or': 'ADDOP',
+        }
 
-tokens = ['DIGITS','NUM','LETTERS','RELOP','ADDOP','MULOP','ID','ASSIGNOP','COMMENT','DOT'
-          ] + list(reserved.values())
+        tokens = ['DIGITS','NUM','LETTERS','RELOP','ADDOP','MULOP','ID','ASSIGNOP','COMMENT','DOT','COLON','LBRACKET','RBRACKET','LPAREN','RPAREN',
+                'COM','POINT','SEMICOLON','EQUAL'
+                ] + list(reserved.values())
 
-literals = [';', '.', '(', ')', ',', ':', '[', ']', '\'']
+        t_EQUAL=r'='
+        t_COLON = r':'
+        t_LBRACKET = r'\['
+        t_RBRACKET = r'\]'
+        t_LPAREN = r'\('
+        t_RPAREN = r'\)'
+        t_COM = r','
+        t_POINT = r'\.'
+        t_DOT=r'\.\.'
+        t_SEMICOLON = r';'
+        t_ASSIGNOP=r':='
+        t_RELOP = r'<=|>=|<>|<|>'
+        t_ADDOP = r'(?i)\+|-'
+        t_MULOP = r'(?i)\*|\/'
+        t_DIGITS=r'\d+'
+        def t_LETTERS(t):
+                r'\'[^\']*\'*'
+                t.value=t.value[1:] if len(t.value)!=1 else 'eof' #获取引号内内容
+                if t.value[-1]!='\'' or t.value=='eof': #引号未关闭
+                    self.error.append({
+                        "code": "A-05",
+                                "info": {
+                                    "line": t.lineno,
+                                    "value": ['Letter_Eof'],
+                                    "lexpos": t.lexpos
+                    }})
+                    t.value=t.value[0] #取第一个字符为值
+                elif t.value=='\'': #字符常量为空
+                    self.error.append({
+                        "code": "A-06",
+                                "info": {
+                                    "line": t.lineno,
+                                    "value": ['Letter_Empty'],
+                                    "lexpos": t.lexpos
+                    }})
+                    t.value="\0" #取"\0"为值
+                elif '\n'in t.value:#字符常量先遇见换行符而非引号
+                    self.error.append({
+                        "code": "A-07",
+                                "info": {
+                                    "line": t.lineno,
+                                    "value": ['Letter_newline'],
+                                    "lexpos": t.lexpos
+                    }})
+                    index=t.value.find('\n')
+                    t.lexer.skip(index-len(t.value)) #前进至换行符前
+                    t.value=t.value[0] if t.value[0]!='\n' else "\0" #错误恢复,取第一个字符或"\0"为值
+                else:
+                    t.value=t.value[:-1]
+                return t
+        
+        def t_ID(t):
+            r'[0-9a-zA-Z_][a-zA-Z_0-9]*'
+            t.type = dict(reserved, **reserved_2).get(t.value,'ID')    # Check for reserved words
 
-def t_DOT(t):
-    r'\.\.'
-    return t
+            if t.value.isdigit():
+                    t.type='DIGITS'
+            if(t.type=='ID'):
+                if len(t.value)>=20:
+                    self.error.append({
+                                "code": "A-03",
+                                "info": {
+                                    "line": t.lineno,
+                                    "value": [t.value.split('\n')[0]],
+                                    "lexpos": t.lexpos
+                                }
+                            })
+                    t.value=t.value[:20]
+                if t.value[0].isdigit() :  # 出现ID以数字开头的错误
+                            self.error.append({
+                                "code": "A-01",
+                                "info": {
+                                    "line": t.lineno,
+                                    "value": [t.value.split('\n')[0]],
+                                    "lexpos": t.lexpos
+                                }
+                            })
+                            while t.value[0].isdigit():
+                                t.value = t.value[1:]  # 错误恢复：如果ID首元素是数字，则去掉该数字
+            return t
 
-def t_ASSIGNOP(t):
-    r':='
-    return t
+        def t_COMMENT(t):
+            r'\{[^{}]*\}*'
+            t.lexer.lineno += t.value.count('\n')
+            if t.value[-1]!='}':
+                self.error.append({
+                        "code": "A-08",
+                            "info": {
+                                "line": t.lineno,
+                                "value": ['Comment_Eof'],
+                                "lexpos": t.lexpos
+                    }})   
+                t.lexer.skip(t.value.find('\n')-len(t.value))
+            pass
+            # No return value. Token discarded
 
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value,'ID')    # Check for reserved words
-    return t
+        # Define a rule so we can track line numbers
+        def t_newline(t):
+            r'\n+'
+            t.lexer.lineno += t.value.count('\n')
 
-def t_DIGITS(t):
-    r'\d+'
-    return t
+        # Compute column.
+        # input is the input text string
+        # token is a token instance
 
-def t_LETTERS(t):
-    r"'[^'\n]'"
-    return t
+        # Error handling rule
+        def t_error(t):
+                    self.error.append({  # 不在已有错误中，则为词法分析中的非法字符错误
+                        "code": "A-02",
+                        "info": {
+                            "line": t.lineno,
+                            "value": [t.value.split('\n')[0]],
+                            "lexpos": t.lexpos
+                        }
+                    })
+                    t.lexer.skip(1)  # 错误处理：跳过该错误
 
-def t_RELOP(t):
-    r'>=|<=|<>|<|>|='
-    return t
-
-def t_ADDOP(t):
-    r'\+|-|or'
-    return t
-
-def t_MULOP(t):
-    r'\*|/|and|div|mod'
-    return t
-
-def t_COMMENT(t):
-    r'\{[^{}]*\}|//.*'
-    pass
-    # No return value. Token discarded
-
-# Define a rule so we can track line numbers
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-# Compute column.
-# input is the input text string
-# token is a token instance
-def find_column(input,token):
-    last_cr = data.rfind('\n',0,token.lexpos)
-    if last_cr < 0:
-        last_cr = 0
-    column = token.lexpos - last_cr
-    return column
-
-# Error handling rule
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-t_ignore  = ' \t'
-
-# Build the lexer
-lexer = lex.lex()
-
-# Test it out
-data = '''
-3 + 4 * 10
-    + -{548548}20 *2
-    array[0..9,0..1]
-    a=2.1
-a= 0aq
-'''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: break      # No more input
-    print (tok, tok.type, tok.value, tok.lineno, find_column(tok.value,tok))
+        t_ignore  = ' \t'
+        # Build the lexer
+        lexer = lex.lex()
+    def find_column(data,token):
+            last_cr = data.rfind('\n',0,token.lexpos)
+            if last_cr < 0:
+                last_cr = 0
+            column = token.lexpos - last_cr
+            return column
+    def Lexical(self,filename):
+        # Build the lexer
+        lexer = lex.lex()
+        filename = filename + ".pas"
+        data = '''
+        const {vfsab
+            b='
+            c='a
+    '''
+        # Give the lexer some input
+        lexer.input(data)
+        # Tokenize
+        while True:
+            tok = lexer.token()
+            if not tok:
+                    break      # No more input
+            else:
+                if self.find_column(data, tok)  < 1000:
+                    self.ans.append(tok)
+                else:
+                    self.error.append({
+                        "code": "A-02",
+                        "info": {
+                            "line": 0,
+                            "value": ["代码行长度过长"],
+                            "lexpos": 0}
+                            })
+                    exit()
